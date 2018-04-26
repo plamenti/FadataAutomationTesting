8f2d8eb0da1c52f237e9eb7fa36dedb9efc37909
plamen.kostadinov <plamen.kostadinov@fadata.eu>
2018-04-26 10:34:01 +0300

    Add Methods demo

---
+++ MethodsDemo/src/com/plamenti/Main.java
@@ -0,0 +1,37 @@
+package com.plamenti;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String clientName = "Pesho";
+        String carBrand = "BMW";
+        String carModel = "320";
+        double carValue = 100000;
+        int carAge = 5;
+        boolean isBmw = true;
+
+        calculatePremium(clientName, carValue, carAge, isBmw);
+
+        clientName = "Gosho";
+        carBrand = "Audi";
+        carModel = "A4";
+        carValue = 120000;
+        carAge = 2;
+        isBmw = false;
+
+        calculatePremium(clientName, carValue, carAge, isBmw);
+    }
+
+    public static void calculatePremium(String clientName, double carValue, int carAge, boolean isBmw){
+        double basePremium = 0.06;
+        double ageBase = 0.01;
+        double isBmwValue = 0.00;
+
+        if(isBmw){
+            isBmwValue = 0.01;
+        }
+
+        double premium = (carValue * basePremium )+ (carValue * carAge * ageBase) + (carValue * isBmwValue);
+        System.out.println(clientName + " Premium: " + premium);
+    }
+}
